3e854c558a6ea6d9099c2b6cc43c014aa67a23ac
BubblesToTheLimit <mond.beton@googlemail.com>
2017-06-15 18:05:12 +0200

    Makeup changes
    Added a catch for possible matrix limit violations
    Added comments and a debug message

--- InterpolationTransformation.m
+++ InterpolationTransformation.m
@@ -4,11 +4,22 @@ rMax=1;
 Mr= 512*2;
 Nr= Mr; 
 
-for i=1:anz_Max-2
-    imP= C(:,werte_MaxMax(i):werte_MaxMax(i+1));
+for i=1:numel(werte_MaxMax)-1
+    lbound = werte_MaxMax(i);
+    ubound = werte_MaxMax(i+1);
+    % make sure ubound and lbound are within the limits of C
+    [m,n] = size(C);
+    if lbound > n || ubound > n
+        disp("exiting prematurely because lower or upper bound exceeds picture limits")
+        break;
+    end
+    % make a selection on the original image to then transform its
+    % coordinates
+    imP= C(:,lbound:ubound);
     
     imR(1:1024,(i-1)*1024+1:i*1024)= PolarToIm (imP, rMin, rMax, Mr, Nr);
 
+    % disp(["displaying picture from " + num2str(lbound) + " to " + num2str(ubound)])
     figure('name','in kartesischen Koordinaten')
     colormap gray;
     imagesc(imR(1:1024,(i-1)*1024+1:i*1024));
