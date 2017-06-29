79e1a32d9b37270c994760a85ef3a066a56bed6f
BubblesToTheLimit <mond.beton@googlemail.com>
2017-06-29 13:59:46 +0200

    Filter_Artefacts läuft nun sauber durch

--- PBL_Filter_Artefacts.m
+++ PBL_Filter_Artefacts.m
@@ -11,7 +11,7 @@
 medfilt2_iteration_Th = 4; % amount of times medfilt2 should be executed
 im2bw_Th_1 = 0.53; % threshhold for first im2bw
 gaussfilt_Th = 6; % amount of times to execute gaussian filter
-im2bw_Th_2 = 0.47; % threshhold for second im2bw
+im2bw_Th_2 = 0.45; % threshhold for second im2bw
 %% 
 % Get the original image to make sure we are not working ontop of modifications 
 % from "gapfinder".
@@ -24,56 +24,48 @@ C = histeq(C); %possible alternative: imadjust(), gives worse results though
 colormap gray;
 imagesc(C);
 %% 
-% Smooth it out
+% Smooth it out and binarize it
 
 for i = 1:medfilt2_iteration_Th
     C = medfilt2(C,[3,3]);
 end
-imagesc(C)
-%% 
-% Run Binaryze on it.
-
-BW = im2bw(C, im2bw_Th_1);
-imagesc(BW);
-%% 
-% Try different filters for getting rid of noise.
-
-D_fixed=im2double(C);
-Z = imgaussfilt(D_fixed, gaussfilt_Th);
-imagesc(Z)
-%% 
-% Run Binaryze on it (again).
-
-d_BW = im2bw(Z, im2bw_Th_2); % the higher the value the blacker the picture
-figure; % attempt to make imagesc actually display something
+C = im2bw(C, im2bw_Th_1);
 colormap gray;
-imagesc(d_BW);
-%%  
-% Filtering the Artefact Line between Row 250 and 190 (expects Gapfinder 
-% to be executed before)
+imagesc(C);
+%% 
+% Try different filters for getting rid of noise and binarize it again
 
-Artefact1 = im2double(d_BW);
-Artefact_mask = zeros(size(Artefact1));
-myfinish=2000; %finish-10;
-for i = myfinish : -1 : 1 
+% C = im2double(C);
+% C = imgaussfilt(C, gaussfilt_Th);
+% C = im2bw(C, im2bw_Th_2); % the higher the value the blacker the picture
+% colormap gray;
+% imagesc(C);
+%%  
+% Filtering the Artefact Line between Row 250 and 190
+
+C = imcomplement(C);
+C = im2double(C);
+Artefact_mask = zeros(size(C));
+[m,n] = size(C);
+for i = n-10 : -1 : 1 
     for j =  250 :-1: 190
         % überprüfe die beiden Punkte darunter auf weiß und einen Punkt 10 weiter rechts von dem schwarzen Punkt ausgehend
-        if((Artefact1(j,i)==0) && (Artefact1(j + 1,i) == 1) && (Artefact1(j + 2,i) == 1) && (Artefact1(j+2,i+10) == 1)) 
-            Artefact1(j,i) = 1;
+        if((C(j,i)==0) && (C(j + 1,i) == 1) && (C(j + 2,i) == 1) && (C(j+2,i+10) == 1)) 
+            C(j,i) = 1;
             Artefact_mask(j,i) = 1;
         end
     end
-    if (round(3*myfinish/4) == i) 
-        disp('25% erreicht') 
+    if (round(3*n/4) == i) 
+        disp('25% der Artefakte gefiltert') 
     end
-    if (round(myfinish/2) == i) 
-        disp('50% erreicht') 
+    if (round(n/2) == i) 
+        disp('50% der Artefakte gefiltert') 
     end
-    if (round(myfinish/4) == i) 
-        disp('75% erreicht') 
+    if (round(n/4) == i) 
+        disp('75% der Artefakte gefiltert') 
     end
     if (2 == i) 
-        disp('100% erreicht') 
+        disp('100% der Artefakte gefiltert') 
     end
 end
 imagesc(Artefact_mask);
@@ -98,15 +90,16 @@ imagesc(Artefact_mask);
 % end
 % disp("100% erreicht")
 %% 
-% Show previously computed mask
+% Apply and show the previously computed mask
 
+C = original;
 grey_val = 0.4; % todo manueller wert
-for j = 250 : 1 : 190
-    for i = 1 : 1 : myfinish
+for j = 250 : -1 : 190
+    for i = 1 : 1 : n
         if (Artefact_mask(j,i) == 1)
-            original(j,i) = grey_val;
+            C(j,i) = grey_val;
         end
     end
 end
 colormap gray;
-imagesc(original);
+imagesc(C);
